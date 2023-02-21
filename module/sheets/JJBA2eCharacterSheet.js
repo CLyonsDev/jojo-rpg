04036e81a41f237d3317777452d12d571705ef37
CLyonsDev <brickmaster12@gmail.com>
2023-02-20 22:47:07 -0800

    Cleaned up unused packages

--- module/sheets/JJBA2eCharacterSheet.js
+++ module/sheets/JJBA2eCharacterSheet.js
@@ -3,15 +3,73 @@ export default class JJBA2eCharacterSheet extends ActorSheet {
         return mergeObject(super.defaultOptions, {
             template: "systems/jojo-rpg/templates/standuser-sheet.hbs",
             classes: ["jjba2e", "sheet", "namedCharacter", "character-scripts"],
-            width: 690,
-            height:810,
-            tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "stand"}]
+            width: 750,
+            height: 810,
+            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "stand" }]
         });
     }
 
     get template() {
+        console.log("getTemplate");
+        console.log(this.getData());
         return "systems/jojo-rpg/templates/standuser-sheet.hbs";
     }
+
+    /*render() {
+        const context = super.render();
+
+        console.log(context);
+        const doc = context.form;
+
+        console.log(doc);
+
+        var mychart = doc.getElementById("statChart").getContext("2d");
+
+        var chart_options = {
+            responsive: true,
+            scales: {
+                r: {
+                    pointLabels: {
+                        display: false
+                    },
+                    ticks: {
+                        display: false,
+                        beginAtZero: true,
+                        stepSize: 1,
+                        max: 5
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+
+        var chart_data = [1, 5, 3, 2, 4, 1];
+
+        console.log("init chart");
+        var round_graph = new Chart(mychart, {
+            type: 'radar',
+            data: {
+                labels: ['Power', 'Potential', 'Precision', 'Speed', 'Range', 'Durability'],
+                datasets: [{
+                    label: 'Ability Stats',
+                    data: chart_data,
+                    borderCapStyle: 'round',
+                    fill: true,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    pointBackgroundColor: 'rgb(255, 99, 132)',
+                    pointBorderColor: '#fff',
+                    pointHoverBackgroundColor: '#fff',
+                    pointHoverBorderColor: 'rgb(255, 99, 132)'
+                }]
+            },
+            options: chart_options
+        });
+    }*/
 
     activateListeners(html) {
         super.activateListeners(html);
@@ -36,5 +94,7 @@ export default class JJBA2eCharacterSheet extends ActorSheet {
             // Handle the click event
             createChart();
         });
+
+        //window.addEventListener('load', createChart());
     }
 }
--- scripts/character-scripts.js
+++ scripts/character-scripts.js
@@ -1,3 +1,52 @@
+function createChart() {
+    var mychart = document.getElementById("statChart").getContext("2d");
+
+    var chart_options = {
+        responsive: true,
+        scales: {
+            r: {
+                pointLabels: {
+                    display: false
+                },
+                ticks: {
+                    display: false,
+                    beginAtZero: true,
+                    stepSize: 1,
+                    max: 5
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+
+    var chart_data = [1, 5, 3, 2, 4, 1];
+
+    console.log("init chart");
+    var round_graph = new Chart(mychart, {
+        type: 'radar',
+        data: {
+            labels: ['Power', 'Potential', 'Precision', 'Speed', 'Range', 'Durability'],
+            datasets: [{
+                label: 'Ability Stats',
+                data: chart_data,
+                borderCapStyle: 'round',
+                fill: true,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgb(255, 99, 132)',
+                pointBackgroundColor: 'rgb(255, 99, 132)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgb(255, 99, 132)'
+            }]
+        },
+        options: chart_options
+    });
+}
+
 async function alterMomentum(actId, modifier) {
     const act = game.actors.get(actId);
 
